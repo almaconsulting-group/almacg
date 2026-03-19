5c62f9248b63d689c3987bc078ccdcec0c0ded83
saraviaa <saraviatomas006@gmail.com>
2026-03-18 22:14:32 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from "vite"; // Cambiamos vitest por vite para el build principal
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
 export default defineConfig({
-  base: "/", // Esto le dice a Vite que cargue todo desde la raíz del dominio almacg.com.ar
+
   plugins: [react()],
+
   test: {
+
     environment: "jsdom",
+
     globals: true,
+
     setupFiles: ["./src/test/setup.ts"],
+
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
+
   },
+
   resolve: {
+
     alias: { "@": path.resolve(__dirname, "./src") },
+
   },
+
 });
