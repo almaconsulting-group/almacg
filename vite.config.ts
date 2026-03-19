04bef3f4b74083f8fda4f39803e971d42f7b7c03
saraviaa <saraviatomas006@gmail.com>
2026-03-18 22:19:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Base "/" es crucial para que funcione con tu dominio propio almacg.com.ar
   base: "/",
   plugins: [react()],
   build: {
-    // Especificamos la carpeta de salida que GitHub Actions espera
     outDir: "dist",
   },
   test: {
